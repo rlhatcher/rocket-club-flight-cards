fb6b4cfed3460b562e6f7eff47e15276643c5647
rlhatcher <ronaldhatcher@mac.com>
2021-05-22 16:22:54 +0100

    files

--- pages/cards/[id].js
+++ pages/cards/[id].js
@@ -1,4 +1,13 @@
 import Layout from '../../components/layout'
+import { getAllCardIds } from '../../lib/cards'
+
+export async function getStaticPaths() {
+  const paths = getAllCardIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
 
 export default function Card() {
   return <Layout>...</Layout>
