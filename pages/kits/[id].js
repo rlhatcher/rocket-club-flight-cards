6250293b026d402dc53c11272d9b698385abfa07
rlhatcher <ronaldhatcher@mac.com>
2021-07-30 10:49:14 +0100

    static view working

--- pages/kits/[id].js
+++ pages/kits/[id].js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Date from "../../components/date";
 import Layout from "../../components/layout";
-import { getAllKitIds, getKitData } from "../../lib/cards";
+import { getAllKitIds, getKitData } from "../../lib/kits";
+import { PaperClipIcon } from '@heroicons/react/solid'
 
 export async function getStaticProps({ params }) {
   const kitData = getKitData(params.id);
@@ -25,65 +26,46 @@ export default function Kit({ kitData }) {
   return (
     <Layout>
       <Head>
-        <title>{cardData.name}</title>
+        <title>{kitData.mfg.name} - {kitData.name}</title>
       </Head>
-      <div className="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
-        <div className="px-4 py-5 sm:px-6">
-          {cardData.name}
-          {/* We use less vertical padding on card headers on desktop than on body sections */}
+      <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+      <div className="flex items-center">
+        <div>
+        <img
+          className="inline-block h-14 w-14 rounded-md"
+          src={kitData.image}
+          alt={kitData.name}
+        />
         </div>
-        <div className="px-4 py-5 sm:p-6">
-          {" "}
-          {cardData.name}
-          <br />
-          {cardData.motor}
-          <br />
-          {cardData.state}
-          <br />
-          {cardData.closeDate}
+        <div>
+        <h3 className="text-lg leading-6 font-medium text-gray-900">{kitData.name}</h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">{kitData.mfg.name}</p>
         </div>
       </div>
-    </Layout>
-  );
-
-/* This example requires Tailwind CSS v2.0+ */
-import { PaperClipIcon } from '@heroicons/react/solid'
-
-export default function Example() {
-  return (
-        <Layout>
-        <Head>
-          <title>{cardData.name}</title>
-        </Head>
-    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">Applicant Information</h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
       </div>
       <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
         <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
           <div className="sm:col-span-1">
-            <dt className="text-sm font-medium text-gray-500">Full name</dt>
-            <dd className="mt-1 text-sm text-gray-900">Margot Foster</dd>
+            <dt className="text-sm font-medium text-gray-500">Recommended Engines</dt>
+            <dd className="mt-1 text-sm text-gray-900">{kitData.recommended_engines}</dd>
           </div>
           <div className="sm:col-span-1">
-            <dt className="text-sm font-medium text-gray-500">Application for</dt>
-            <dd className="mt-1 text-sm text-gray-900">Backend Developer</dd>
+            <dt className="text-sm font-medium text-gray-500">Projected Altitude</dt>
+            <dd className="mt-1 text-sm text-gray-900">{kitData.projected_max_altitude}</dd>
           </div>
           <div className="sm:col-span-1">
-            <dt className="text-sm font-medium text-gray-500">Email address</dt>
-            <dd className="mt-1 text-sm text-gray-900">margotfoster@example.com</dd>
+            <dt className="text-sm font-medium text-gray-500">Length</dt>
+            <dd className="mt-1 text-sm text-gray-900">{kitData.length}</dd>
           </div>
           <div className="sm:col-span-1">
-            <dt className="text-sm font-medium text-gray-500">Salary expectation</dt>
-            <dd className="mt-1 text-sm text-gray-900">$120,000</dd>
+            <dt className="text-sm font-medium text-gray-500">Recovery System</dt>
+            <dd className="mt-1 text-sm text-gray-900">{kitData.recovery_system}</dd>
           </div>
           <div className="sm:col-span-2">
-            <dt className="text-sm font-medium text-gray-500">About</dt>
+            <dt className="text-sm font-medium text-gray-500">Description</dt>
             <dd className="mt-1 text-sm text-gray-900">
-              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
-              qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
-              pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+              {kitData.description}
             </dd>
           </div>
           <div className="sm:col-span-2">
@@ -119,8 +101,5 @@ export default function Example() {
       </div>
     </div>
     </Layout>
-  )
-}
-
-
+  );
 }
