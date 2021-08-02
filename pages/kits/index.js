1a21c4621c11e7e98697bdac552e9b365500fff1
rlhatcher <ronaldhatcher@mac.com>
2021-08-02 18:20:36 +0100

    temp

--- pages/kits/index.js
+++ pages/kits/index.js
@@ -2,23 +2,24 @@ import Link from "next/link";
 import Date from "../../components/date";
 import Head from "next/head";
 import Layout, { siteTitle } from "../../components/layout";
-import { getFlightCardData } from "../../lib/cards";
+import { getKitData } from "../../lib/kits";
 import {
   CalendarIcon,
   FireIcon,
   LocationMarkerIcon,
 } from "@heroicons/react/solid";
+import { BadgeCheckIcon } from "@heroicons/react/outline";
 
 export async function getStaticProps() {
-  const cardData = getFlightCardData();
+  const kitData = getKitData();
   return {
     props: {
-      cardData,
+      kitData,
     },
   };
 }
 
-export default function Home({ cardData }) {
+export default function Kits({ kitData }) {
   return (
     <Layout>
       <div className="bg-white overflow-hidden shadow rounded-lg">
@@ -29,7 +30,7 @@ export default function Home({ cardData }) {
                 <div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
                   <div className="ml-4 mt-2">
                     <h3 className="text-lg leading-6 font-medium text-gray-900">
-                      Flight Cards
+                      Kits
                     </h3>
                   </div>
                   <div className="ml-4 mt-2 flex-shrink-0">
@@ -37,53 +38,50 @@ export default function Home({ cardData }) {
                       type="button"
                       className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                      Create new card
+                      Build a Kit
                     </button>
                   </div>
                 </div>
               </div>
               <ul className="divide-y divide-gray-200">
-                {cardData.map((card) => (
-                  <li key={card.id}>
-                    <Link href={`/cards/${card.id}`}>
+                {kitData.map((kit) => (
+                  <li key={kit.id}>
+                    <Link href={`/cards/${kit.id}`}>
                       <div className="flex items-center px-4 py-4 sm:px-6">
                         <div className="min-w-0 flex-1 flex items-center">
                           <div className="flex-shrink-0">
                             <img
                               className="h-12 w-12 rounded-full"
-                              src={application.applicant.imageUrl}
+                              src={kit.image}
                               alt=""
                             />
                           </div>
                           <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
                             <div>
                               <p className="text-sm font-medium text-indigo-600 truncate">
-                                {application.applicant.name}
+                                {kit.name}
                               </p>
                               <p className="mt-2 flex items-center text-sm text-gray-500">
-                                <MailIcon
+                                <OfficeBuildingIcon
                                   className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
                                   aria-hidden="true"
                                 />
                                 <span className="truncate">
-                                  {application.applicant.email}
+                                  {kit.mfg.name}
                                 </span>
                               </p>
                             </div>
                             <div className="hidden md:block">
                               <div>
                                 <p className="text-sm text-gray-900">
-                                  Applied on{" "}
-                                  <time dateTime={application.date}>
-                                    {application.dateFull}
-                                  </time>
+                                  {kit.length} long and {kit.diameter} in diameter
                                 </p>
                                 <p className="mt-2 flex items-center text-sm text-gray-500">
-                                  <CheckCircleIcon
+                                  <BadgeCheckIcon
                                     className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
                                     aria-hidden="true"
                                   />
-                                  {application.stage}
+                                  Recommended Motors {kit.recommended_engines}
                                 </p>
                               </div>
                             </div>
