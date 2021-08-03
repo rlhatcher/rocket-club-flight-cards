f2d14fe1252f8faaf8a568f734adcf6be77e65a0
rlhatcher <ronaldhatcher@mac.com>
2021-08-03 21:42:22 +0100

    stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../lib/apolloClient'
 import { UserProvider } from '@auth0/nextjs-auth0'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps)
+
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </UserProvider>
   );
 }
